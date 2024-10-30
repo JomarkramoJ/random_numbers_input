3817c12ee8cb54f295f250f4740d5994bc3dbdf3
Jomark <maguadjomarkglenn@gmail.com>
2024-10-31 05:24:59 +0800

    added something to prevent duplicates

--- random_numbers_input.py
+++ random_numbers_input.py
@@ -11,19 +11,24 @@ while True:
     try:
         input_number = int(input("Please enter any random number ranging from 1 to 50: "))
         if input_number >= 1 and input_number <= 10:
-                num_range["First_set"].append(input_number)
+                if input_number not in num_range["First_set"]:
+                        num_range["First_set"].append(input_number)
                 continue
         elif input_number >= 11 and input_number <= 20:
-                num_range["Second_set"].append(input_number)  
+                if input_number not in num_range["Second_set"]:
+                        num_range["Second_set"].append(input_number)
                 continue
         elif input_number >= 21 and input_number <= 30:
-                num_range["Third_set"].append(input_number)
+                if input_number not in num_range["Third_set"]:
+                        num_range["Third_set"].append(input_number)
                 continue
         elif input_number >= 31 and input_number <= 40:
-                num_range["Fourth_set"].append(input_number)
+                if input_number not in num_range["Fourth_set"]:
+                        num_range["Fourth_set"].append(input_number)
                 continue
         elif input_number >= 41 and input_number <= 50:
-                num_range["Fifth_set"].append(input_number)
+                if input_number not in num_range["Fifth_set"]:
+                        num_range["Fifth_set"].append(input_number)
                 continue
         else:
                 print("Invalid Input")
@@ -31,6 +36,6 @@ while True:
     except:
            print("Input Error")
 
-#new variable to get all the items inside the dictionary and then print
+#New variable to get all the items inside the dictionary and then print
 for set_name, numbers in num_range.items():
     print(set_name + ":", *numbers, sep=", ")
